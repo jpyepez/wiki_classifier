26c6bfa87a8c5ab221903795c9e91bc863cf9aad
JP <jpyepezimc@gmail.com>
2018-10-17 01:56:05 +1300

    adding files

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const classifier = require('./classifier');
+
+const container = document.getElementById('container');
+
+// classifier.classifyRndDog(container);
+classifier.classifyTestCat(container);
+
+classifier.classifyRndDog(container);
