2647ac422627d29d3c713ffc83c3adca18ec3f93
JP <jpyepezimc@gmail.com>
2018-11-14 10:46:22 +1300

    Revert "limited to 10 results"
    This reverts commit 7377b0fc37d7833e683ee48221ebd6949f45e61c.

--- src/js/wikipedia.js
+++ src/js/wikipedia.js
@@ -14,7 +14,6 @@ export const findImages = async (key) => {
         let images = await page.images();
         images = images.filter(isImageUrl);
         images = images.filter(isJPG);
-        images = images.slice(0, 10);
         return images;
     } catch (error) {
         container.innerHTML = "";
